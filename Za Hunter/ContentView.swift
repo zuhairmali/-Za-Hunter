328010dc7e8b5802d76b7baa8400c53181af5fbb
Zuhair Ali <zuhairali20003@gmail.com>
2021-04-26 00:35:19 -0500

    Initial Commit

---
+++ Za Hunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Za Hunter
+//
+//  Created by Student on 4/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Student on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
